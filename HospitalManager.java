19d2b651c2d0743a5736e6ca4936661a11c02144
BBK-PiJ-2015-85 <picklesj85@gmail.com>
2015-11-11 15:32:35 +0000

    Added code for delete patient, queue length, and doubly linked lists.

--- HospitalManager.java
+++ HospitalManager.java
@@ -4,11 +4,45 @@ public class HospitalManager {
 	public void launch() {
 		Patient p1 = new Patient("John", 37, "Swine Flu");
 		patientListStart = p1;
-		patientListStart.addPatient(new Patient("Sam", 30, "Common cold"));
-		patientListStart.addPatient(new Patient("Adam", 29, "Sore throat"));
-		patientListStart.addPatient(new Patient("Jane", 30, "broken leg"));
-		patientListStart.addPatient(new Patient("Cath", 30, "swollen knee"));
-		patientListStart.printPatient();
+		Patient p2 = new Patient("Sam", 30, "Common cold");
+		patientListStart.addPatient(p2);
+		Patient p3 = new Patient("Adam", 29, "Sore throat");
+		patientListStart.addPatient(p3);
+		Patient p4 = new Patient("Jane", 30, "broken leg");
+		patientListStart.addPatient(p4);
+		Patient p5 = new Patient("Cath", 30, "swollen knee");
+		patientListStart.addPatient(p5);
+		
+		
+		/*patientListStart.printPatientForwards();		
+		System.out.println();		
+		System.out.println(patientListStart.queueLength());
+		patientListStart.deletePatient("Adam");
+		System.out.println(patientListStart.queueLength());
+		patientListStart.deletePatient("Cath");
+		System.out.println(patientListStart.queueLength());
+		patientListStart = p2;
+		patientListStart.printPatientForwards();
+		System.out.println(patientListStart.queueLength());*/
+		
+		patientListStart.printPatientForwards();
+		System.out.println("*****");		
+		p5.printPatientBackwards();
+		System.out.println("*****");
+		patientListStart.deletePatient("Sam");
+		patientListStart.deletePatient("Adam");
+		patientListStart.printPatientForwards();
+		System.out.println("*****");		
+		p5.printPatientBackwards();
+		Patient p6 = new Patient("Dave", 54, "T.B.");
+		patientListStart.addPatient(p6);
+		patientListStart.deletePatient("Sam");
+		patientListStart.printPatientForwards();
+		System.out.println("*****");
+		p6.printPatientBackwards();
+
+
+		
 	}
 	
 	public static void main(String[] args) {
--- Patient.java
+++ Patient.java
@@ -3,6 +3,7 @@ public class Patient {
 	private int age;
 	private String illness;
 	private Patient nextPatient;
+	private Patient previousPatient;
 	
 	public Patient(String name, int age, String illness){
 		this.name = name;
@@ -14,17 +15,52 @@ public class Patient {
 	public void addPatient(Patient newPatient){
 		if (this.nextPatient == null) {
 			this.nextPatient = newPatient;
+			newPatient.previousPatient = this;
 		} else {
 			this.nextPatient.addPatient(newPatient);
 		}
 	}
 	
-	public void printPatient() {
+	public void printPatientForwards() {
 		System.out.println("This patient is called: " + this.name + ". They are " + this.age + " years old. Their illness is: " + this.illness + ".");
 		if(this.nextPatient != null) {
-			this.nextPatient.printPatient();		
+			this.nextPatient.printPatientForwards();		
 		}
 	}
 	
-	public boolean 
+	public void printPatientBackwards() {
+		System.out.println("This patient is called: " + this.name + ". They are " + this.age + " years old. Their illness is: " + this.illness + ".");
+		if (this.previousPatient != null) {
+			this.previousPatient.printPatientBackwards();
+		}
+	}
+	
+	public int queueLength() {
+		//recursive
+		if (this.nextPatient == null){
+			return 1;
+		} else {
+			return 1 + this.nextPatient.queueLength();
+		}
+		
+		//iterative 
+		/*int count = 1;
+		for (Patient p = this; p.nextPatient != null; p = p.nextPatient) {
+			count++;
+		}
+		return count;*/
+	}
+	
+	public void deletePatient(String patientName) {
+		if (this.nextPatient == null) {
+			System.out.println("Patient not found.");
+			return;
+		} else if (this.nextPatient.name.equals(patientName)) {
+			this.nextPatient = this.nextPatient.nextPatient;
+			this.nextPatient.previousPatient = this.nextPatient.previousPatient.previousPatient;
+			System.out.println(patientName + " has been deleted.");
+		} else {
+			this.nextPatient.deletePatient(patientName);
+		}
+	}
 }
