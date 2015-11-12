eeb9f6e9814f6753bcd5fe4f5296b4294c4b2adb
BBK-PiJ-2015-85 <picklesj85@gmail.com>
2015-11-12 14:04:15 +0000

    improved code so that the list is in the order that patients are added. did this using iteration rather than recusion.

--- CircularPatient.java
+++ CircularPatient.java
@@ -2,47 +2,52 @@ public class CircularPatient {
 	private String name;
 	private int age;
 	private CircularPatient nextPatient;
-	private boolean last;
+	
 	
 	public CircularPatient(String name, int age) {
 		this.name = name;
 		this.age = age;
-		this.nextPatient = this;
-		this.last = false;
+		this.nextPatient = this;		
 	}
 	
 	public void addPatient(CircularPatient newPatient) {
-		
+		CircularPatient start = this;
 		if (this.nextPatient == this) {
 			this.nextPatient = newPatient;
 			newPatient.nextPatient = this;
-			newPatient.last = true;
 		} else {
-			newPatient.nextPatient = this.nextPatient;
-			this.nextPatient = newPatient;			
-		}		
+			for (CircularPatient p = this.nextPatient; p != start; p = p.nextPatient) {
+				if (p.nextPatient == start) {
+					p.nextPatient = newPatient;
+					newPatient.nextPatient = this;
+				}
+			}			
+		}
 	}
 	
 	public void printList() {
 		System.out.println("Patient name: " + this.name + ". Patient age: " + this.age + ".");
-		if (this.last == false) {
-			this.nextPatient.printList();
+		CircularPatient start = this;		
+		for (CircularPatient p = this.nextPatient; p != start; p = p.nextPatient) {
+			System.out.println("Patient name: " + p.name + ". Patient age: " + p.age + ".");
 		}
-		
 	}
 	
 	public void deletePatient(String patientName) {
-		if (this.last == true) {
-			System.out.println("Patient not found.");
-			return;
-		} else if (this.nextPatient.name.equals(patientName)) {
-			if (this.nextPatient.last == true) {
-				this.last = true;
+		CircularPatient start = this;
+		for (CircularPatient p = this.nextPatient; p != start; p = p.nextPatient){
+			if (p.nextPatient == start) {
+				System.out.println("Patient " + patientName + " not found.");
+				break;
+			} else if (p.nextPatient.name.equals(patientName)) {
+				p.nextPatient = p.nextPatient.nextPatient;
+				System.out.println(patientName + " has been deleted.");
+				break;
+			} else if (start.nextPatient.name.equals(patientName)) {
+				start.nextPatient = start.nextPatient.nextPatient;
+				System.out.println(patientName + " has been deleted.");
+				return;
 			}
-			this.nextPatient = this.nextPatient.nextPatient;
-			System.out.println(patientName + " has been deleted.");			
-		} else {
-			this.nextPatient.deletePatient(patientName);
 		}
 	}
 }
