bd6d8894d02471f2cb4fed11fc221199ee1263fa
BBK-PiJ-2015-85 <picklesj85@gmail.com>
2015-11-13 16:04:23 +0000

    see commit for patient.java

--- HospitalManager.java
+++ HospitalManager.java
@@ -1,46 +1,122 @@
 public class HospitalManager {
 	private Patient patientListStart = null;
 	
+	public HospitalManager() {
+		this.patientListStart = null;
+	}
+	
+	public void addPatient(String name, int age, String illness) {
+		Patient newPatient = new Patient(name, age, illness);
+		if (this.patientListStart == null) {
+			this.patientListStart = newPatient;
+		} else {
+			Patient current = patientListStart;
+			while (current.getNext() != null) {
+				current = current.getNext();
+			}
+			current.setNext(newPatient);
+			newPatient.setPrevious(current);
+		}		
+	}
+	
+	public void deletePatient(String name) {
+		if (patientListStart.getName().equals(name)) {
+			patientListStart = patientListStart.getNext();
+			patientListStart.setPrevious(null);
+			System.out.println("Patient " + name + " deleted.");
+			return;
+		} else {
+			Patient current = patientListStart;
+			while (current != null && !current.getName().equals(name)) {
+				current = current.getNext();
+			}
+			if (current == null) {
+				System.out.println("Patient " + name + " not found.");
+				return;
+			} else {
+				current.getPrevious().setNext(current.getNext());
+				if (current.getNext() != null) {
+					current.getNext().setPrevious(current.getPrevious());
+				}
+				System.out.println("Patient " + name + " deleted.");
+			}
+		}		
+	}
+	
+	public void printList() {
+		Patient current = patientListStart;
+		if (current == null) {
+			System.out.println("The list is empty.");
+		}
+		while (current != null) {
+			System.out.println("Patient: " + current.getName() + ". Age: " + current.getAge() + ". Illness: " + current.getIllness() + ".");
+			current = current.getNext();
+		}
+	}
+	
+	public void printListBackwards() {
+		Patient current = patientListStart;
+		while (current.getNext() != null) {
+			current = current.getNext(); 
+		}		
+		while (current != null) {
+			System.out.println("Patient: " + current.getName() + ". Age: " + current.getAge() + ". Illness: " + current.getIllness() + ".");
+			current = current.getPrevious();
+		}
+	}
+	
+	public int listLength() {
+		int count = 0;
+		Patient current = patientListStart;
+		while (current != null) {
+			count++;
+			current = current.getNext();
+		}
+		return count;
+	}
+	
 	public void launch() {
-		Patient p1 = new Patient("John", 37, "Swine Flu");
-		patientListStart = p1;
-		Patient p2 = new Patient("Sam", 30, "Common cold");
-		patientListStart.addPatient(p2);
-		Patient p3 = new Patient("Adam", 29, "Sore throat");
-		patientListStart.addPatient(p3);
-		Patient p4 = new Patient("Jane", 30, "broken leg");
-		patientListStart.addPatient(p4);
-		Patient p5 = new Patient("Cath", 30, "swollen knee");
-		patientListStart.addPatient(p5);
+		addPatient("John", 22, "Swine Flu");
+		addPatient("Sam", 30, "Common cold");
+		addPatient("Adam", 29, "Sore throat");
+		addPatient("Jane", 30, "broken leg");
+		addPatient("Cath", 30, "swollen knee");
 		
+		System.out.println(listLength());
 		
-		patientListStart.printPatientForwards();		
-		System.out.println();		
-		System.out.println(patientListStart.queueLength());
-		patientListStart.deletePatient("Adam");
-		System.out.println(patientListStart.queueLength());
-		patientListStart.deletePatient("Jane");
-		System.out.println(patientListStart.queueLength());
-		patientListStart = p2;
-		patientListStart.deletePatient("John");
-		patientListStart.printPatientForwards();
-		System.out.println(patientListStart.queueLength());
+		printList();
+		System.out.println("*****************************");
+		printListBackwards();
+		System.out.println("*****************************");
+		deletePatient("John");
+		System.out.println(listLength());
+		printList();
+		System.out.println("*****************************");
+		printListBackwards();
+		deletePatient("Sam");
+		System.out.println(listLength());
+		printList();	
+		System.out.println("*****************************");
+		printListBackwards();
+		deletePatient("Adam");
+		System.out.println(listLength());
+		printList();
+		System.out.println("*****************************");
+		printListBackwards();
+		deletePatient("Cath");
+		System.out.println(listLength());
+		printList();
+		System.out.println("*****************************");
+		printListBackwards();
 		
+		addPatient("Dave", 39, "Fever");
+		addPatient("Gwen", 98, "arthritis");
+		System.out.println(listLength());
+		printList();
+		System.out.println("*****************************");
+		printListBackwards();
 		
-		System.out.println("*****");		
-		p5.printPatientBackwards();
-		System.out.println("*****");
-		patientListStart.deletePatient("Sam");
-		patientListStart.deletePatient("Adam");
-		patientListStart.printPatientForwards();
-		System.out.println("*****");	
-		patientListStart.printPatientBackwards();
-		Patient p6 = new Patient("Dave", 54, "T.B.");
-		patientListStart.addPatient(p6);
-		patientListStart.deletePatient("Sam");
-		patientListStart.printPatientForwards();
-		System.out.println("*****");
-		patientListStart.printPatientBackwards();
+		deletePatient("Adam");
 		
 		
 		
