c1770e8394d185b2b8316964bfa11c0e2e48f6ba
BBK-PiJ-2015-85 <picklesj85@gmail.com>
2015-11-13 16:03:04 +0000

    multiple improvements. changed the structure of the two classes and used getters and setters, implement everything iteratively and improved delete so no unwanted pointers are left.

--- Patient.java
+++ Patient.java
@@ -2,70 +2,42 @@ public class Patient {
 	private String name;
 	private int age;
 	private String illness;
-	private Patient nextPatient;
-	private Patient previousPatient;
+	private Patient next;
+	private Patient previous;
 	
 	public Patient(String name, int age, String illness){
 		this.name = name;
 		this.age = age;
 		this.illness = illness;
-		this.nextPatient = null;
+		this.next = null;
+		this.previous = null;
 	}
 	
-	public void addPatient(Patient newPatient){
-		if (this.nextPatient == null) {
-			this.nextPatient = newPatient;
-			newPatient.previousPatient = this;
-		} else {
-			this.nextPatient.addPatient(newPatient);
-		}
+	public String getName() {
+		return name;
 	}
 	
-	public void printPatientForwards() {
-		System.out.println("This patient is called: " + this.name + ". They are " + this.age + " years old. Their illness is: " + this.illness + ".");
-		if(this.nextPatient != null) {
-			this.nextPatient.printPatientForwards();		
-		}
+	public int getAge() {
+		return age;
 	}
 	
-	public void printPatientBackwards() {
-		if (this.nextPatient != null) {
-			this.nextPatient.printPatientBackwards();
-		} else {
-			for (Patient p = this; p != null; p = p.previousPatient) {
-				System.out.println("This patient is called: " + p.name + ". They are " + p.age + " years old.");
-			}
-		}
+	public String getIllness() {
+		return illness;
 	}
 	
-	public int queueLength() {
-		/*recursive
-		if (this.nextPatient == null){
-			return 1;
-		} else {
-			return 1 + this.nextPatient.queueLength();
-		}*/
-		
-		//iterative 
-		int count = 0;
-		for (Patient p = this; p != null; p = p.nextPatient) {
-			count++;
-		}
-		return count;
+	public Patient getNext(){
+		return next;
 	}
 	
-	public void deletePatient(String patientName) {
-		if (this.nextPatient == null) {
-			System.out.println("Patient not found.");
-			return;
-		} else if (this.nextPatient.name.equals(patientName)) {
-			this.nextPatient = this.nextPatient.nextPatient;
-			if (this.nextPatient != null) {
-				this.nextPatient.previousPatient = this.nextPatient.previousPatient.previousPatient;
-			}
-			System.out.println(patientName + " has been deleted.");
-		} else {
-			this.nextPatient.deletePatient(patientName);
-		}
+	public Patient getPrevious() {
+		return previous;
+	}
+	
+	public void setNext(Patient next) {
+		this.next = next;
+	}
+	
+	public void setPrevious(Patient previous) {
+		this.previous = previous;
 	}
 }
